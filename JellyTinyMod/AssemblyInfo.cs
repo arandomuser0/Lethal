0e8b98e5a8563e17925d95a3efa0d444e20ae6b2
arandomuser0 <90565424+arandomuser0@users.noreply.github.com>
2024-03-14 20:28:30 -0300

    Add files via upload

---
+++ JellyTinyMod/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JellyJamMadness")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JellyJamMadness")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f4957b33-0535-4113-88c1-e378a5fc3e2f")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ JellyTinyMod/ModBase.cs
@@ -0,0 +1,34 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: JellyJamMadness.ModBase
+// Assembly: JellyJamMadness, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C7B2576F-62FF-4EAE-A49A-0C18CFDDA436
+// Assembly location: C:\Users\tania\Downloads\JellyJamMadness.dll
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+#nullable disable
+namespace JellyJamMadness
+{
+  [BepInPlugin("JellyJam.MadnessMod", "JellyJamMadness Mod", "1.0.0.0")]
+  public class ModBase : BaseUnityPlugin
+  {
+    private const string modGUID = "JellyJam.MadnessMod";
+    private const string modName = "JellyJamMadness Mod";
+    private const string modVersion = "1.0.0.0";
+    private readonly Harmony harmony = new Harmony("JellyJam.MadnessMod");
+    private static ModBase Instance;
+    internal ManualLogSource mls;
+
+    private void Awake()
+    {
+      if (Object.op_Equality((Object) ModBase.Instance, (Object) null))
+        ModBase.Instance = this;
+      this.mls = Logger.CreateLogSource("JellyJam.MadnessMod");
+      this.mls.LogInfo((object) "The JellyJam mod has started");
+      this.harmony.PatchAll();
+    }
+  }
+}
